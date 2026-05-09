b4ad5a14d440a9687f72792c7ca9629e740b9088
Voice659 <E.R.Fahretdinov@yandex.ru>
2026-05-09 21:06:34 +0300

    Initial release

---
+++ Extra-Code.js
@@ -0,0 +1,22 @@
+console.log("This is a dump for random code!")
+console.log("Some early previews will be here!")
+
+var PD1 = function (){
+    99 * 123;
+    "Long string!".slice(0,4)
+    true && false;
+    12345 + 56789;
+    22 + 33 + 44;
+    1000 - 17;
+    123 * 456;
+    12345 / 250;
+    1234 + 57 * 3 - 31 / 4;
+    8 / 1 + 3;
+    console.log("Fix:")
+    8 / (1+3)
+    15 + 9 * 2
+    console.log("Fix:")
+    (15+9) * 2
+}
+
+PD1()
---
+++ HB-JS.js
@@ -0,0 +1,7 @@
+var P1 = function (repeats) {
+    for (var i = 0; i < repeats; i++){
+        console.log(i + ". =^.^=")
+    }
+}
+
+P1(10)
